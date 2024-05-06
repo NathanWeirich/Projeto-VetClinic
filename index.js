fff0abf48ab827222e9aab2ea628e28ca00db45d
Nathan Weirich <nathanweirich@gmail.com>
2024-05-06 18:46:24 -0300

    feat(tutor): add post route

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ const Pet = require("./models/Pet");
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.get("/tutor", (req, res) => {
+  res.render("addtutor");
+});
+
+app.post("/tutor", function (req, res) {
+  const name = req.body.name;
+  const phone = req.body.phone;
+  const email = req.body.email;
+  const date_of_birth = req.body.date_of_birth;
+  const zip_code = req.body.zip_code;
+  Tutor.create({ name, phone, email, date_of_birth, zip_code })
+    .then(res.redirect("/"))
+    .catch((err) => console.log(err));
+});
+
 app.get("/", (req, res) => {
   res.render("home");
 });
